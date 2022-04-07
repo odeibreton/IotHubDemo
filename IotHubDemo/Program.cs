0ed7e0dd20472852131df9cc97a1738695c5fb66
Odei Bretón <odeibreton@hotmail.com>
2022-04-07 12:17:42 +0100

    Add project files.

---
+++ IotHubDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Azure.Devices.Client;
+using System.Text;
+using System.Text.Json;
+
+// Connection string of the device
+const string connectionString = "<connection string>";
+
+// Example message to send
+var text = JsonSerializer.Serialize(new
+{
+    Message = "This is an example message"
+});
+
+using var client = DeviceClient.CreateFromConnectionString(connectionString);
+
+using var message = new Message(Encoding.UTF8.GetBytes(text))
+{
+    ContentEncoding = "utf-8",
+    ContentType = "application/json"
+};
+
+await client.SendEventAsync(message);
